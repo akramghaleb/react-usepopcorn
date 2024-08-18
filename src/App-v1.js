8d5b8e72364aae3cc46210026b410f301ea4b4c2
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-08-18 09:30:11 +0300

    useEffect to the Rescue

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,23 +50,10 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = '33fa474c'
-
 // ----------------------------------------------------------
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-
-  useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.Response === "True") {
-          console.log(data)
-          setMovies(data.Search);
-        }
-      })
-  }, [])
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
